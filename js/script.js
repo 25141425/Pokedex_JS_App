1974257a22c659e937ad1b561605da9a892022a5
25141425 <bennykaltenbach@gmail.com>
2024-08-21 20:54:21 +0200

    Created pokemonList

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-alert('Hello world');
-
-let favoriteFood = 'Schnitzel';
-document.write(favoriteFood);
+let pokemonList = [
+  {name: 'Beedrill', height: 1, types: ['Bug', 'Poison'], weight: 29.5},
+  {name: 'Jigglypuff', height: 0.5, types: ['Fairy', 'Normal'], weight: 5.5},
+  {name: 'Pidgeotto', height: 1.1, types: ['Flying', 'Normal'], weight: 30},
+  {name: 'Dewgong', height: 1.7, types: ['Ice', 'Water'], weight: 120}
+ ];
