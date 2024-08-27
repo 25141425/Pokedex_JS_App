dea66e86c9e5a5bb2cfbae1de15e5e2ccc82c43c
25141425 <bennykaltenbach@gmail.com>
2024-08-27 14:03:19 +0200

    Wrapped pokemonList in IIFE and updated loop code

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
-// Creating array of objects
-let pokemonList = [
+let pokemonRepository = (function () {
+  let pokemonList = [
   {name: 'Beedrill', height: 1, types: ['Bug', 'Poison'], weight: 29.5},
   {name: 'Jigglypuff', height: 0.5, types: ['Fairy', 'Normal'], weight: 5.5},
   {name: 'Pidgeotto', height: 1.1, types: ['Flying', 'Normal'], weight: 30},
   {name: 'Dewgong', height: 1.7, types: ['Ice', 'Water'], weight: 120}
  ];
- 
- pokemonList.forEach(function(item) {
+
+  return {
+    add: function(item) {
+      pokemonList.push(item);
+    },
+    getAll: function() {
+      return pokemonList;
+    }
+  };
+})();
+
+pokemonRepository.add({ name: 'Gloom', height: 0.8, types: ['Grass', 'Poison'], weight: 8.6});
+
+pokemonRepository.getAll().forEach(function(item) {
   if (item.height > 1.5) {
-   document.write(`<p> ${item.name} (height: ${item.height}) - Wow, that's big! </p>`);
+    document.write(`<p> ${item.name} (height: ${item.height}) - Wow, that's big! </p>`);
   }
   else {
-    document.write(`<p> ${item.name} (height: ${item.height}) </p>`);
+    document.write(`<p> ${item.name} (height: ${item.height})`);
   }
- })
+});
