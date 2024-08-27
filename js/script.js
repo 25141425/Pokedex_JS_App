66a619690e7b23a224a55c60d56bf19d5a09d692
25141425 <bennykaltenbach@gmail.com>
2024-08-27 13:57:37 +0200

    Using forEach instead of for loop

--- js/script.js
+++ js/script.js
@@ -6,13 +6,11 @@ let pokemonList = [
   {name: 'Dewgong', height: 1.7, types: ['Ice', 'Water'], weight: 120}
  ];
  
- /* First line is looping through pokemonList array as long as i is smaller than the number of objects in the array.
- Conditional checks if height > 1.5. If so, "Wow, that's big!" is being added in the print on the HTML.
- <p> tag is added in document.write so every Pokemon appears on new line.
-*/
- for (let i = 0; i < pokemonList.length; i++)  
-    if (pokemonList[i].height > 1.5) { 
-  document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) - Wow, that's big!</p>`); } 
-  else { 
-    document.write(`<p> ${pokemonList[i].name} (height: ${pokemonList[i].height}) </p>`); 
+ pokemonList.forEach(function(item) {
+  if (item.height > 1.5) {
+   document.write(`<p> ${item.name} (height: ${item.height}) - Wow, that's big! </p>`);
   }
+  else {
+    document.write(`<p> ${item.name} (height: ${item.height}) </p>`);
+  }
+ })
